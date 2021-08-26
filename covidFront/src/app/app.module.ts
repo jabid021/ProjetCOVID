015d646c00a9e34efe1d755994cdf1dda81bd7c1
Jordan <Jordanabid@gmail.com>
2021-08-26 16:03:18 +0200

    merge raph

--- covidFront/src/app/app.module.ts
+++ covidFront/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UtilisateurComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
