cc7b2e07052b436f136b2c758abd1022a2868279
Jordan <Jordanabid@gmail.com>
2021-08-26 15:29:51 +0200

    Angular classes : Lieu,Creneau,Motif,Consultation,Specialite

---
+++ covidFront/src/app/model/specialite.ts
@@ -0,0 +1,7 @@
+import {Practicien} from "./practicien";
+
+export class Specialite
+{
+  constructor(id?:number,version?:number,nom?:string,description?:string,practiciens?:Array<Practicien>) {
+  }
+}
