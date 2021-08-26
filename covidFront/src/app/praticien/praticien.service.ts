fd2d521b75da75f2385c276f58ce4c74633752a9
Unknown <aurelien.dispot@gmail.com>
2021-08-27 01:25:42 +0200

    cc

--- covidFront/src/app/praticien/praticien.service.ts
+++ covidFront/src/app/praticien/praticien.service.ts
@@ -3,18 +3,18 @@ import {AppConfigService} from "../app-config.service";
 import {HttpClient} from "@angular/common/http";
 import {Praticien} from "../model/praticien";
 import {Observable} from "rxjs";
+import {AccueilComponent} from "../accueil/accueil.component";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PraticienService {
-
   praticiens: Array<Praticien> = new Array<Praticien>();
-
+  recherche:string;
   chemin: string;
-  constructor(private http : HttpClient,private appconfig:AppConfigService) {
+  constructor(private http : HttpClient,private appconfig:AppConfigService, private acceuilComponent:AccueilComponent) {
+    this.recherche = this.acceuilComponent.recherche;
     this.chemin = this.appconfig.backEndUrl + "praticien/";
-    this.load();
   }
 
   findAll(): Array<Praticien> {
@@ -25,9 +25,17 @@ export class PraticienService {
     return this.http.get<Praticien>(this.chemin + id+"/with-specialite");
   }
 
-  findAllPraticienWithSpecialites() {
-    this.http.get<Array<Praticien>>(this.appconfig.backEndUrl + "praticien/with-specialites/").subscribe(response => {
+  findAllPraticienWithSpecialites(): Array<Praticien> {
+    let temp: Array<Praticien> = new Array<Praticien>();
+    this.http.get<Array<Praticien>>(this.chemin +"with-specialites/").subscribe(response => {
       this.praticiens = response;
+      for (var indice = 0; indice < this.praticiens.length; indice++) {
+        if (this.praticiens[indice].nom == this.recherche) {
+          temp.push(this.praticiens[indice]);
+        }
+      }
+      this.praticiens = temp;
+      return this.praticiens;
     }, error => console.log(error));
   }
 
