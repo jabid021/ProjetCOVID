6a6467a3b144c0683dfce07fb0b6a95c379f6f98
Damienhb <snowdarkwinter@gmail.com>
2021-08-27 09:04:00 +0200

    ListeRdv

--- covidFront/src/app/app-routing.module.ts
+++ covidFront/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PatientRdvPasseComponent} from "./patient-rdv-passes/patient-rdv-passe.component";
+import {PatientRdvAVenirComponent} from "./patient-rdv-avenir/patient-rdv-avenir.component";
 
-const routes: Routes = [];
+let PatientRdvAVenirComponentComponent;
+const routes: Routes = [
+  {path: "app-patient-rdv-passes", component: PatientRdvPasseComponent},
+  {path: "app-patient-rdv-avenir", component: PatientRdvAVenirComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- covidFront/src/app/app.module.ts
+++ covidFront/src/app/app.module.ts
@@ -3,17 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientRdvPasseComponent } from './patient-rdv-passes/patient-rdv-passe.component';
+import { PatientRdvAVenirComponent } from './patient-rdv-avenir/patient-rdv-avenir.component';
+import { PatientComponent } from './patient/patient.component';
 import {PraticienInfosPersoComponent} from "./practicien-infos-perso/praticien-infos-perso.component";
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PraticienInfosPersoComponent
+    PatientRdvPasseComponent,
+    PatientRdvAVenirComponent,
+    PatientComponent,
+    PraticienInfosPersoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
   ],
   providers: [],
